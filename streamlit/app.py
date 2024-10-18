61c23edf2d897b8a2c17de9ed8ffbbb4ec969446
Carlos <101848698+Kiwiabacaxi@users.noreply.github.com>
2024-10-18 09:34:37 -0300

    updatw 2

--- streamlit/app.py
+++ streamlit/app.py
@@ -7,52 +7,52 @@ planilha = {
     'Sprint 1': pd.DataFrame({
         'Tarefa': ['H1', 'H2', 'H3', 'H4'],
         'Junior': [2, 1, 1, 0],
-        'Pleno': [0, 0, 0, 0],
-        'Senior': [0, 0, 0, 0],
+        'Pleno': [1, 0, 0, 0],
+        'Senior': [1, 0, 0, 0],
         'Total': [2.5, 1.5, 1, 0],
         'Custo Junior': [595.24, 178.57, 119.05, 0],
-        'Custo Pleno': [0, 0, 0, 0],
-        'Custo Senior': [0, 0, 0, 0]
+        'Custo Pleno': [231.90, 0, 0, 0],
+        'Custo Senior': [368.33, 0, 0, 0]
     }),
     'Sprint 2': pd.DataFrame({
         'Tarefa': ['H1', 'H2', 'H3', 'H4'],
         'Junior': [4, 2, 1, 1],
-        'Pleno': [0, 0, 0, 0],
-        'Senior': [0, 0, 0, 0],
+        'Pleno': [1, 0, 0, 0],
+        'Senior': [1, 0, 0, 0],
         'Total': [3, 1.5, 1.5, 1.5],
         'Custo Junior': [1428.57, 357.14, 178.57, 178.57],
-        'Custo Pleno': [0, 0, 0, 0],
-        'Custo Senior': [0, 0, 0, 0]
+        'Custo Pleno': [231.90, 0, 0, 0],
+        'Custo Senior': [368.33, 0, 0, 0]
     }),
     'Sprint 3': pd.DataFrame({
         'Tarefa': ['H1', 'H2', 'H3', 'H4'],
         'Junior': [2, 1, 1, 0],
-        'Pleno': [0, 0, 0, 0],
-        'Senior': [0, 0, 0, 0],
+        'Pleno': [1, 0, 0, 0],
+        'Senior': [1, 0, 0, 0],
         'Total': [2, 1.5, 2, 0],
         'Custo Junior': [476.19, 178.57, 238.10, 0],
-        'Custo Pleno': [0, 0, 0, 0],
-        'Custo Senior': [0, 0, 0, 0]
+        'Custo Pleno': [231.90, 0, 0, 0],
+        'Custo Senior': [368.33, 0, 0, 0]
     }),
     'Sprint 4': pd.DataFrame({
         'Tarefa': ['H1', 'H2', 'H3', 'H4'],
         'Junior': [4, 2, 2, 0],
-        'Pleno': [0, 0, 0, 0],
-        'Senior': [0, 0, 0, 0],
+        'Pleno': [1, 0, 0, 0],
+        'Senior': [1, 0, 0, 0],
         'Total': [3, 1, 2, 0],
         'Custo Junior': [1428.57, 238.10, 476.19, 0],
-        'Custo Pleno': [0, 0, 0, 0],
-        'Custo Senior': [0, 0, 0, 0]
+        'Custo Pleno': [231.90, 0, 0, 0],
+        'Custo Senior': [368.33, 0, 0, 0]
     }),
     'Sprint 5': pd.DataFrame({
         'Tarefa': ['H1', 'H2', 'H3', 'H4'],
         'Junior': [2, 2, 0, 0],
-        'Pleno': [0, 0, 0, 0],
-        'Senior': [0, 0, 0, 0],
+        'Pleno': [1, 0, 0, 0],
+        'Senior': [1, 0, 0, 0],
         'Total': [2.5, 2, 0, 0],
         'Custo Junior': [595.24, 476.19, 0, 0],
-        'Custo Pleno': [0, 0, 0, 0],
-        'Custo Senior': [0, 0, 0, 0]
+        'Custo Pleno': [231.90, 0, 0, 0],
+        'Custo Senior': [368.33, 0, 0, 0]
     })
 }
 
@@ -60,15 +60,15 @@ planilha = {
 custos_release = {
     'Mão de Obra': {
         'Junior': 7142.86,
-        'Pleno': 0,
-        'Senior': 0,
+        'Pleno': 2319.00,
+        'Senior': 3683.30,
         'SM (1/N)': 0,
         'PO (1/N)': 0,
-        'Total MO': 7142.86
+        'Total MO': 13145.16
     },
-    'Lucro Empresa': 3571.43,  # 50% do custo de mão de obra
-    'Custos Fixos': 1428.57,    # 20% do custo de mão de obra
-    'Valor da Release': 12142.86
+    'Lucro Empresa': 6572.58,  # 50% do custo de mão de obra
+    'Custos Fixos': 2629.03,    # 20% do custo de mão de obra
+    'Valor da Release': 22346.77
 }
 
 # Criar uma interface Streamlit
@@ -127,8 +127,8 @@ else:
 
         # Calcular o custo total da sprint
         custo_junior = total_junior * 119.05  # Valor exemplo por hora
-        custo_pleno = total_pleno * 178.57    # Valor exemplo por hora
-        custo_senior = total_senior * 238.10   # Valor exemplo por hora
+        custo_pleno = total_pleno * 231.90    # Valor atualizado por hora
+        custo_senior = total_senior * 368.33   # Valor atualizado por hora
 
         custo_total = custo_junior + custo_pleno + custo_senior
 
